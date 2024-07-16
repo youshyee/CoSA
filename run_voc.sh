858d3516cd0b1ad2b3c04386e56ad1f83fc6d03d
Xinyu <youshyee@outlook.com>
2024-07-16 03:25:40 +0100

    para update

--- run_voc.sh
+++ run_voc.sh
@@ -4,6 +4,8 @@ NAME=EXP_VOC
 EXP_DIR=$HOME/CoSA_EXP
 RANDPORT=6531
 echo Using port $RANDPORT
-torchrun --master_port $RANDPORT --nproc_per_node=1 main.py $NAME --work_dir $EXP_DIR \
+torchrun --master_port $RANDPORT --nproc_per_node=2 main.py $NAME --work_dir $EXP_DIR \
 	--dataset VOC12 \
 	--voc12_root $HOME/data/VOCdevkit/VOC2012 \
+	--max_iters 32000 \
+	--aux_layer -4 \
